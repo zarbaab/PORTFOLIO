d2ed4ca441e783eafe96aa5f1421674157383906
zarbaab <147990676+zarbaab@users.noreply.github.com>
2024-08-24 01:09:04 +0500

    Done!!

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,124 @@
+import React from "react";
+import HTML from "../../public/html.png";
+import CSS from "../../public/css.jpg";
+import JS from "../../public/javascript.png";
+import java from "../../public/java.png";
+import python from "../../public/python.webp";
+import mongoDB from "../../public/mongodb.jpg";
+import Figma from "../../public/figma.png";
+import AdobeXd from "../../public/xd.png";
+import Vite from "../../public/vite.svg";
+import reactjs from "../../public/reactjs.png";
+import nodejs from "../../public/node.png";
+import Canva from "../../public/canva.png";
+import Photoshop from "../../public/ps.png";
+import Oracle from "../../public/oracle.png";
+
+function PortFolio() {
+  const cardItem = [
+    {
+        id: 1,
+        logo: HTML,
+        name: "html",
+      },
+      {
+        id: 2,
+        logo: CSS,
+        name: "css",
+      },
+      {
+        id: 3,
+        logo: JS,
+        name: "js",
+      },
+      {
+        id: 4,
+        logo: reactjs,
+        name: "ReactJS",
+      },
+    {
+      id: 5,
+      logo: mongoDB,
+      name: "MongoDB",
+    },
+   
+    {
+      id: 6,
+      logo: nodejs,
+      name: "NodeJS",
+    },
+    {
+      id: 7,
+      logo: python,
+      name: "Python",
+    },
+    {
+      id: 8,
+      logo: java,
+      name: "Java",
+    },
+    {
+        id:9 ,
+        logo:Figma ,
+        name: "figma",
+      },
+      {
+        id:10 ,
+        logo: AdobeXd,
+        name: "XD",
+      },
+      {
+        id: 11,
+        logo: Canva,
+        name: "canva",
+      },
+     
+      {
+        id:13 ,
+        logo:Photoshop ,
+        name: "photoshop",
+      },
+    
+  ];
+  return (
+    <div
+      name="Portfolio"
+      className="max-w-screen-2xl container mx-auto px-4 md:px-20 mt-10"
+    >
+      <div>
+        <h1 className="text-3xl font-bold mb-5">PortFolio</h1>
+        <span className=" underline font-semibold">Featured Projects</span>
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-3 my-5">
+          {cardItem.map(({ id, logo, name }) => (
+            <div
+              className="md:w-[300px] md:h-[300px] border-[2px] rounded-lg shadow-lg p-1 cursor-pointer hover:scale-110 duration-300"
+              key={id}
+            >
+              <img
+                src={logo}
+                className="w-[120px] h-[120px] p-1 rounded-full border-[2px]"
+                alt=""
+              />
+              <div>
+                <div className="px-2 font-bold text-xl mb-2">{name}</div>
+                <p className="px-2 text-gray-700">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                </p>
+              </div>
+              <div className=" px-6 py-4 space-x-3 justify-around">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 py-2 rounded">
+                  Video
+                </button>
+                <button className="bg-green-500 hover:bg-green-700 text-white font-bold px-4 py-2 rounded">
+                  Source code
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PortFolio;
