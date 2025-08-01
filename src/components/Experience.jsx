39ec171ea0aa64a499b320b6c35b72f02552b351
Zarbaab Mehboob <zarbaabmehboob@gmail.com>
2025-08-01 02:14:52 -0700

    Fixed CSS syntax and updated components

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
 import html from "../../public/html.png";
 import css from "../../public/css.jpg";
 import java from "../../public/java.png";
@@ -62,73 +61,7 @@ function Experience() {
     },
   ];
 
-=======
-import html from "/html.png";
-import css from "/css.jpg";
-import java from "/java.png";
-import javascript from "/javascript.png";
-import Figma from "/figma.png";
-import Reactjs from "/reactjs.png";
-import Python from "/python.webp";
-import AdobeXD from "/xd.png";
-import Tailwind from "/tailwind.png";
-import canva from "/canva.png";
 
-function Experience() {
-  const cardItem = [
-    {
-      id: 1,
-      logo: html,
-      name: "HTML",
-    },
-    {
-      id: 2,
-      logo: css,
-      name: "CSS",
-    },
-    {
-      id: 3,
-      logo: java,
-      name: "Java",
-    },
-    {
-      id: 4,
-      logo: javascript,
-      name: "JavaScript",
-    },
-    {
-      id: 5,
-      logo: Python,
-      name: "Python",
-    },
-    {
-      id: 6,
-      logo: Figma,
-      name: "Figma",
-    },
-    {
-      id: 7,
-      logo: AdobeXD,
-      name: "AdobeXD",
-    },
-    {
-      id: 8,
-      logo: canva,
-      name: "canva",
-    },
-    {
-      id: 9,
-      logo: Reactjs,
-      name: "Reactjs",
-    },
-    {
-      id: 10,
-      logo: Tailwind,
-      name: "Tailwind",
-    },
-   
-  ];
->>>>>>> 32cd73a9204deb87b207fe92b65cf0cac774225c
   return (
     <div
       name="Experience"
@@ -136,7 +69,6 @@ function Experience() {
     >
       <div>
         <h1 className="text-3xl font-bold mb-5">Experience</h1>
-<<<<<<< HEAD
         <p className="text-gray-700 mb-10">
           I have over 2 years of experience working with the following tools and technologies:
         </p>
@@ -157,24 +89,7 @@ function Experience() {
             </div>
           </div>
         ))}
-=======
-        <p className="  ">
-        As a Software Engineering Student, I'm excited to apply my skills and knowledge in Designing and Development. Although I'm new to the professional world, I've had hands-on experience with various technologies, including:
-        </p>
-        <div className="grid grid-cols-2 md:grid-cols-5 gap-7 my-3">
-          {cardItem.map(({ id, logo, name }) => (
-            <div
-              className=" flex flex-col items-center justify-center border-[2px] rounded-full md:w-[200px] md:h-[200px] shadow-md p-1 cursor-pointer hover:scale-110 duration-300"
-              key={id}
-            >
-              <img src={logo} className="w-[150px] rounded-full" alt="" />
-              <div>
-                <div className="">{name}</div>
-              </div>
-            </div>
-          ))}
-        </div>
->>>>>>> 32cd73a9204deb87b207fe92b65cf0cac774225c
+
       </div>
     </div>
   );
